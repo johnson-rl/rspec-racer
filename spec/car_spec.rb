969e4e7055e28507ec0772a972f4be02bcd8566b
johnson-rl <johnson.rl@gmail.com>
2017-01-30 21:25:24 -0800

    part 1 complete

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
 
 RSpec.describe Car do
+  let(:car) { Car.new }
+
   describe '::new' do
-    it 'sets the initial speed of a car to 0' 
+    it 'sets the initial speed of a car to 0' do
+      expect(car.speed).to eq(0)
+    end
   end
 
   describe '#speed' do
-    it "has a speed getter method"
+    it "has a speed getter method" do
+      expect(car.speed).to be >= 0
+    end
   end
 
   describe '#accelerate' do
-    it 'increases the speed of car by a given amount' 
+    it 'increases the speed of car by a given amount'do
+      car.accelerate(40)
+      expect(car.speed).to eq(40)
+    end
   end
 end
